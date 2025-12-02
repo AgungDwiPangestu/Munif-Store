ed1d9404c29eee5753c1f2b45e04a2b5b8601970
AgungDwiPangestu <agung.dwi23@students.utdi.ac.id>
2025-12-02 22:58:12 +0700

    Update logout functionality

---
+++ pages/logout.php
@@ -0,0 +1,8 @@
+<?php
+require_once '../config/db.php';
+require_once '../config/functions.php';
+
+// Logout user
+session_destroy();
+set_flash('Anda telah logout.', 'success');
+redirect('/Munif/index.php');
